37199a7fd691375f850f212c3298a8b9de8239cd
songyijian <397032724@qq.com>
2017-02-07 19:51:39 +0800

    stat

--- models/builder.js
+++ models/builder.js
@@ -1,10 +1,12 @@
-// 构建处理
+// 构建处理 
 const fs = require("fs");
 const path = require("path");
 const queryString = require("querystring");
 const builderData = require("../set.json");
 exports.builder = (req, res, next) => {
     let alldata = "";
+    let endData = {};
+    //get 数据
     req.on("data", function(chunk) {
         alldata += chunk;
     });
@@ -13,13 +15,11 @@ exports.builder = (req, res, next) => {
     req.on("end", function() {
         let ajaxData = queryString.parse(alldata);
         let cpath = ajaxData.path.split(path.sep);
-
-        let endData = {};
         console.log(cpath)
 
         if (ajaxData.type) {
-            fs.stat(`${ajaxData.type}`, (err, stats) => {
-                if (err) {
+            fs.access(ajaxData.type, err => {
+                if (!err) {
                     endData = {
                         "state": -1,
                         "err": `请检查${ajaxData.type}目录是否存在 || set配置是否正确？`
@@ -28,45 +28,51 @@ exports.builder = (req, res, next) => {
                     res.send(endData)
                     return false;
                 }
-
-                fs.stat(`${ajaxData.type}/${cpath[0]}`, (err, stats) => {
+                fs.stat(`${ajaxData.type}/${ajaxData.path}`, (err, stats) => {
                     if (err) {
-                        console.log("不存在可以创建：",`${ajaxData.type}/${cpath[0]}`)
-                        endData = {
-                            "state": -1,
-                            "iofn": `请检查${ajaxData.type}目录是否存在 || set配置是否正确？`
-                        };
-                        
-                        res.send(endData)
+                        mrFn(cpath, req, res)
                         return false;
                     }
-                    // fs.stat(`${ajaxData.type}/${cpath[0]}`, (err, stats) => {
-                    //     if (err) {
-                    //         console.log("不存在可以创建：",`${ajaxData.type}/${cpath[0]}`)
-                    //         endData = {
-                    //             "state": -1,
-                    //             "iofn": `请检查${ajaxData.type}目录是否存在 || set配置是否正确？`
-                    //         };
-                            
-                    //         res.send(endData)
-                    //         return false;
-                    //     }
-                    // })
-
-
+                    endData = {
+                        "state": -1,
+                        "iofn": `${ajaxData.type}/${ajaxData.path} 该项目已经存在`
+                    };
                     res.send(endData)
-
                 })
-
-
-
-
                 res.send(endData)
             })
         }
+        console.log("//", ajaxData);
 
+        function mrFn(array, req, res) {
+            let p = '';
+            let y = false;
+            array.forEach(function(element, index) {
+                p += element;
+                fs.stat(`${ajaxData.type}/${p}`, (err, stats) => {
+                    if (err) {
+                        fs.mkdir(`${ajaxData.type}/${p}`, element, err => {
+                            if (err) {
+                                endData = {
+                                    "state": -1,
+                                    "err": `${ajaxData.type}/${p} 抱歉构建失败 `
+                                };
+                                console.error(err);
+                                res.send(endData)
+                                return false;
+                            }
 
-        console.log("////", ajaxData);
+                            endData = {
+                                "state": 1,
+                                "info": `${ajaxData.type}/${p}`
+                            };
+                            res.send(endData)
+                        })
+                    }
+                })
+            }, this);
+        }
+
     });
 
 };
