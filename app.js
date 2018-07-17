fdc0b639c223b4685c72eb9b07f70fc720240657
happyelements <happyelements@happyelementsdeMacBook-Pro.local>
2018-07-17 17:36:22 +0800

    url直接获得本地ip手机可直接扫码

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ const express = require("express");
 const app = express();
 const router = require("./controller/router.js");
 const builderData = require("./set.json");
-
 const port = 9000;
-let url = 'http://localhost:' + port;
 
+// 获取本地
+function getIPAdress() {
+    var interfaces = require('os').networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+}
+
+let url = `http://${getIPAdress()}:${port}`;
 app.set("view engine", "ejs");
 app.use('/staticfile', express.static(`staticfile`));
 builderData.ItemType.forEach((item, i) => {
@@ -17,7 +30,6 @@ builderData.ItemType.forEach((item, i) => {
         app.use(`/${www}/${it.id}`, express.static(`${it.path}`, { 'index': [] }));
     })
 });
-console.log('> Listening at ' + url + '\n')
 opn(url)
 
 app.get('/', router.buildershow);
@@ -29,7 +41,8 @@ app.use((req, res) => { res.status(404).send("404!");})
 
 app.listen(port, (err) => {
     if (err) {
-        console.log("本地服务的9000端口可能被占用")
-        console.error(err)
+        console.log("本地服务的9000端口可能被占用",err)
+    }else{
+      console.log('> Listening at ' + url + '\n')
     }
 })
