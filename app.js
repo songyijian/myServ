c0edddb7f9b3dbed2808a68e2f386660243e08e1
yijian.song <happyelements@happyelementsdeMacBook-Pro.local>
2018-09-11 20:25:08 +0800

    上传文件测试接口开发中

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 "use strict"
-const fs = require("fs");
-const opn = require('opn')
 const express = require("express");
 const app = express();
-const router = require("./controller/router.js");
-const mockIndex = require("./mock/index.js");
+const fs = require("fs");
+const opn = require('opn')
 
+const router = require("./controller/router.js");
+const mockrouter = require("./mock/index.js");
 const builderData = require("./set.json");
 const port = 8080;
 
@@ -23,29 +23,40 @@ function getIPAdress() {
     }
 }
 
+//配置corf函数 处理跨域问题
+//全局引用
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    next()
+})
+
+// 目录静态化
 let url = `http://${getIPAdress()}:${port}`;
 app.set("view engine", "ejs");
 app.use('/staticfile', express.static(`staticfile`));
 builderData.ItemType.forEach((item, i) => {
     let www = item.id;
-    item.list.forEach((it, i) => {
-        app.use(`/${www}/${it.id}`, express.static(`${it.path}`, { 'index': [] }));
-    })
+    item.list.forEach((it, i) => { app.use(`/${www}/${it.id}`, express.static(`${it.path}`, { 'index': [] })) })
 });
-opn(url)
+// opn(url) // 自动打开页面
 
 
-
+// builder 功能接口
 app.get('/', router.buildershow);
 app.post('/builder', router.builder);
 app.get('/:typeid/:ckid/*', router.warehouse);
 app.post('/merge', router.merge);
 app.post('/staticv', router.staticv);
-app.get('/mock', mockIndex.test);
+
+
+// mock数据，mock交互接口
+app.use('/mock', mockrouter);
 
 
 
-app.use((req, res) => { res.status(404).send("404!");})
+
+
+app.use((req, res) => { res.status(404).send("404!")})
 app.listen(port, (err) => {
     if (err) {
         console.log(`本地${port}端口可能被占用`,err)
--- mock/index.js
+++ mock/index.js
@@ -1,3 +1,75 @@
-exports.test = (req, res, next) => {
-  res.send('xxxx')
-}
+var express = require('express');
+var router = express.Router();
+var fs = require("fs");
+// var multiparty = require("multiparty");
+var multiparty = require('multiparty');  //图片上传模块  即可以获取form表单的数据 也可以实现上传文件
+
+// 在这个对象下面增加，最后一定要导出
+module.exports = router;
+
+
+//test 测试一下有没有走通
+router.get('/test',(req, res, next)=>{
+  res.send('mocktest 接口是通的')
+})
+
+
+
+router.post('/upfile',(req, res, next)=>{
+    // //获取表单的数据 以及post过来的图片
+    // var form = new multiparty.Form();
+    // form.uploadDir=__dirname+'../upload_box';   //上传图片保存的目录  目录必须存在
+    //
+    // form.parse(req, function(err, fields, files) {
+    //     //获取提交的数据以及图片上传成功返回的图片信息
+    //     console.log(fields);  //获取表单的数据
+    //     console.log(files);  //图片上传成功返回的信息
+    //     // var username=fields.username[0];   //表单字段获取
+    //     // var pic=files.pic[0].path;   //获取文件保存的目录
+    //     res.send(JSON.stringify(fields))
+    //
+    // })
+
+
+        // var form = new multiparty.IncomingForm({
+        //     encoding:"utf-8",
+        //     uploadDir:_dirname+'../upload_box',  //文件上传地址
+        //     keepExtensions:true  //保留后缀
+        // });
+        //
+        // form.parse(req, function(err, fields, files) {
+        //     var obj ={};
+        //     Object.keys(fields).forEach(function(name) {  //文本
+        //         console.log('name:' + name+";filed:"+fields[name]);
+        //         obj[name] = fields[name];
+        //     });
+        //
+        //     Object.keys(files).forEach(function(name) {  //文件
+        //         console.log('name:' + name+";file:"+files[name].path);
+        //         obj[name] = files[name];
+        //     });
+        //
+        //     res.send(err,obj);
+        // })
+
+        //生成multiparty对象，并配置上传目标路径
+        var form = new multiparty.Form({uploadDir: '../upload_box'});
+        //上传完成后处理
+        form.parse(req, function(err, fields, files){
+            // var inputFile = files.file[0];
+            // var uploadedPath = inputFile.path;
+            // var dstPath = './upload_box/' + inputFile.originalFilename;
+            //
+            // fs.rename(uploadedPath, dstPath, function(err) {
+            //     if(err){
+            //         console.log('rename error: ' + err);
+            //     } else {
+            //         console.log('rename ok');
+            //     }
+            // });
+            // files.file.path = dstPath;
+            // var data = files;
+
+            res.send(fields);
+        });
+})
