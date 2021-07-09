1b54c615b00e426911af45607d3e0f112199ab79
yijian.song <yijian.song@opay-inc.com>
2021-07-09 20:17:26 +0800

    perf:优化mock配置

--- mocks/index.js
+++ mocks/index.js
@@ -2,155 +2,17 @@
  * @Description: mock数据入口文件
  * @Author: yijian.song
  * @Date: 2019-08-22 19:23:57
- * @LastEditors: yijian.song
- * @LastEditTime: 2020-07-17 15:34:07
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-07-09 20:14:46
  */
-const Mock = require('mockjs') //如果用到mock语法直接引用
-const Random = Mock.Random
+const { mergeMap } = require("../server/func/tools.js");
+// const Mock = require("mockjs"); //如果用到mock语法直接引用
+// const Random = Mock.Random;
 
-module.exports = new Map()
-  .set(
-    'sdk/dev',
-    ({
-      body,
-      query,
-      method
-    }) => {
-      console.log(body, query, method)
-      return Mock.mock({
-        body,
-        query,
-        method
-      })
-    }
-  )
-  .set('dsp/advertiser/list/', ({
-    body,
-    query,
-    method
-  }) => {
-    return Mock.mock({
-      "code": 200,
-      "msg": "",
-      "data": {
-        "data|5-15": [{
-          a: 1
-        }],
-        "page": [1, 1, 1, 10, 1]
-      }
-    })
-  })
-  .set(
-    'common/log/project_list',
-    ({
-      body,
-      query,
-      method
-    }) => {
-      return {
-        "code": 200,
-        "msg": "",
-        "data": [{
-            "id": 1,
-            "name": "单元管理",
-            "method": {
-              "create":'创建',
-              "update":'geng新',
-              "delete":'删除。。。'
-            },
-            "enable": [
-              {
-                "name": "操作账号",
-                "key": "username",
-                "type": "text"
-              },
-              {
-                "name": "应用ID",
-                "key": "appid",
-                "type": "text"
-              },
-              {
-                "name": "广告单元",
-                "key": "placementid",
-                "type": "text"
-              },
-              {
-                "name": "广告类型",
-                "key": "adtype",
-                "type": "option",
-                "content": [{
-                    "key": 1,
-                    "name": "激励视频"
-                  },
-                  {
-                    "key": 4,
-                    "name": "全屏"
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            "id": 2,
-            "name": "单元管理2",
-            "method": {
-              "create":'创建',
-              "update":'geng新',
-              "delete":'删除。。。'
-            },
-            "enable": [
-              {
-                "name": "操作账号",
-                "key": "username",
-                "type": "text"
-              },{
-                "name": "应用ID",
-                "key": "appid",
-                "type": "text"
-              },
-              {
-                "name": "广告类型",
-                "key": "adtype",
-                "type": "option",
-                "content": [{
-                    "key": '1',
-                    "name": "激励视频"
-                  },
-                  {
-                    "key": '4',
-                    "name": "全屏"
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
+const myMock = new Map().set("test/", () => {
+  return { data: "This is my mock server !!" };
+});
 
-    }
-  )
-  .set(
-    'ssp/log/list',
-    ({
-      body,
-      query,
-      method
-    }) => {
-      return Mock.mock( {
-        "code": 200,
-        "msg": "",
-        "data": {
-          "data|3-10":[{
-            "username": "12121323@simgob.com",
-            "project": "单元管理",
-            "method|1": ['update','delete','create'],
-            "uptime": "2020-07-07 12:12:12",
-            "changes": "变更内容",
-            "context": '{"array": ["Hello","Mock.js","!","Hello","Mock.js","!","Hello","Mock.js","!","Hello","Mock.js","!","Hello","Mock.js","!","Hello","Mock.js","!","Hello","Mock.js","!","Hello","Mock.js","!","Hello","Mock.js","!"]}' 
-          }],
-          pages:[136144, 1, 13615, 10, 1]
-        }
-      })
+const opayConfig = require("./opay");
 
-    }
-  )
+module.exports = mergeMap(myMock, opayConfig);
--- server/controller/cMock.js
+++ server/controller/cMock.js
@@ -1,46 +1,48 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: yijian.song
- * @Version: 
+ * @Version:
  * @Date: 2020-04-01 10:24:48
- * @LastEditors: yijian.song
- * @LastEditTime: 2020-04-09 10:48:13
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-07-09 20:16:16
  */
 
-const {isFunction } = require("../func")
+const { isFunction } = require("../func");
 
 module.exports = {
   // 构建UI
   api: (req, res, next) => {
     try {
-      var mjs = require(req.__CONFIG__.mockfiles+'/index.js')
+      var mjs = require(req.__CONFIG__.mockfiles + "/index.js");
     } catch (error) {
-      console.error(error)
-      throw 'mock文件引入错误'
+      console.error(error);
+      throw "mock文件引入错误";
     }
-    const { body, query, method } = req
-    const routers = req.params[0]  //路由
+    const { body, query, method } = req;
+    const routers = req.params[0]; //路由
     try {
-      let item = mjs.get(routers)
+      let item = mjs.get(routers);
       if (isFunction(item)) {
-        res.send(item({
-          body,   //post携带参数
-          query,  //get携带参数
-          method  //get | post
-        }))
+        res.send(
+          item({
+            body, //post携带参数
+            query, //get携带参数
+            method //get | post
+          })
+        );
       } else {
         res.send({
-          "state": 0,
-          "info": 'mock书写存在问题'
-        })
+          state: 0,
+          info: "mock 存在格式错误！！"
+        });
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
       res.send({
-        "state": 0,
-        "info": '未知错误',
-        "error": error
-      })
+        state: 0,
+        info: "未知错误",
+        error: error
+      });
     }
   }
-}
+};
